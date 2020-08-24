bcef26edbdbeeb4a7fbb9a1a8cc998136b8499ea
haihuanchen <haihuan.chen@hotmail.com>
2020-08-24 18:35:48 -0400

    initial setup files

---
+++ grams/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+
+      <div className='app-header'>
+        <img 
+          className='header-image' 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          alt="instagram header"
+          />
+      </div>
+      
+      <h1>Welcome to Grams App</h1>
+
+
+      {/* Posts */}
+    </div>
+  )
+}
+
+export default App;
